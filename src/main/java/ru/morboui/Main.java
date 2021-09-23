75862896d912fd8162765b41aed9831472b734d6
Dmitry B <lbvf6000@gmail.com>
2021-09-23 12:58:04 +0300

    fix yandex_api_key

--- src/main/java/ru/morboui/Main.java
+++ src/main/java/ru/morboui/Main.java
@@ -21,95 +21,94 @@ import java.util.Date;
 import java.util.List;
 import java.util.TimeZone;
 
-
 public class Main {
 
-    static class Bus {
-        String code;
-        String arrival;
+  static class Bus {
+    String code;
+    String arrival;
 
-
-        public String getCode() {
-            return code;
-        }
-
-        public void setCode(String code) {
-            this.code = code;
-        }
-
-        public void setArrival(String arrival) {
-            this.arrival = arrival;
-        }
-
-        public String getArrival() {
-            return arrival;
-        }
+    public String getCode() {
+      return code;
     }
 
-    static class YA {
-        List<Bus> segments;
-
-        public List<Bus> getSegments() {
-            return segments;
-        }
-
-        public void setSegments(List<Bus> segments) {
-            this.segments = segments;
-        }
-
-        YA() {
-        }
+    public void setCode(String code) {
+      this.code = code;
     }
 
-    public static void main(String[] args) {
-        System.setProperty("file.encoding", "UTF-8");
-
-
-        TelegramBot bot = new TelegramBot(System.getenv("433_BOT_TOKEN"));
-
-        TimeZone tz = TimeZone.getTimeZone("Europe/Moscow");
-        DateFormat df = new SimpleDateFormat("yyyy-MM-dd'T'HH:mm");
-        df.setTimeZone(tz);
-        String nowAsISO = df.format(new Date());
-
-        LocalTime time = LocalTime.parse("12:33");
-
-
-        bot.setUpdatesListener(element -> {
-            //System.out.println(element);
-            element.forEach(it -> {
-
-                HttpClient client = HttpClient.newHttpClient();
-                HttpRequest request = HttpRequest.newBuilder()
-                        .uri(URI.create("https://api.rasp.yandex.net/v3.0/search/?apikey=e28ce556-5d04-48d3-9226-5823b7fc68cb&format=json&from=s9739796&to=s9744794&page=1&date=" + nowAsISO + "&levent=departure"))
-                        .build();
-
-                try {
-                    HttpResponse<String> response = client.send(request, HttpResponse.BodyHandlers.ofString());
-                    ObjectMapper mapper = new ObjectMapper();
-                    mapper.configure(DeserializationFeature.FAIL_ON_UNKNOWN_PROPERTIES, false);
-                    String body = response.body();
-                    //System.out.println(body);
-                    YA ya = mapper.readValue(response.body(), YA.class);
-                    ya.segments.forEach(bus -> {
-                        //bot.execute(new SendMessage(it.message().chat().id(), "Vacancy: "));
-                        String s = bus.arrival;
-                        TemporalAccessor ta = DateTimeFormatter.ISO_INSTANT.parse(s);
-                        Instant i = Instant.from(ta);
-                        Date d = Date.from(i);
-
-                        System.out.println(d + " " + bus.code);
-                    });
-                    response.body();
-                } catch (IOException | InterruptedException e) {
-                    e.printStackTrace();
-                }
-
-
-            });
-
-            return UpdatesListener.CONFIRMED_UPDATES_ALL;
-        });
-
+    public void setArrival(String arrival) {
+      this.arrival = arrival;
     }
+
+    public String getArrival() {
+      return arrival;
+    }
+  }
+
+  static class YA {
+    List<Bus> segments;
+
+    public List<Bus> getSegments() {
+      return segments;
+    }
+
+    public void setSegments(List<Bus> segments) {
+      this.segments = segments;
+    }
+
+    YA() {
+    }
+  }
+
+  public static void main(String[] args) {
+    System.setProperty("file.encoding", "UTF-8");
+
+    TelegramBot bot = new TelegramBot(System.getenv("433_BOT_TOKEN"));
+
+    TimeZone tz = TimeZone.getTimeZone("Europe/Moscow");
+    DateFormat df = new SimpleDateFormat("yyyy-MM-dd'T'HH:mm");
+    df.setTimeZone(tz);
+    String nowAsISO = df.format(new Date());
+
+    LocalTime time = LocalTime.parse("12:33");
+
+    bot.setUpdatesListener(element -> {
+      //System.out.println(element);
+      element.forEach(it -> {
+
+        HttpClient client = HttpClient.newHttpClient();
+        HttpRequest request = HttpRequest.newBuilder()
+            .uri(URI.create(
+                "https://api.rasp.yandex.net/v3.0/search/?apikey=" + System.getenv("YANDEX_API_KEY")
+                    + "b7fc68cb&format=json&from=s9739796&to=s9744794&page=1&date=" + nowAsISO
+                    + "&levent=departure"))
+            .build();
+
+        try {
+          HttpResponse<String> response = client
+              .send(request, HttpResponse.BodyHandlers.ofString());
+          ObjectMapper mapper = new ObjectMapper();
+          mapper.configure(DeserializationFeature.FAIL_ON_UNKNOWN_PROPERTIES, false);
+          String body = response.body();
+          //System.out.println(body);
+          YA ya = mapper.readValue(response.body(), YA.class);
+          ya.segments.forEach(bus -> {
+            //bot.execute(new SendMessage(it.message().chat().id(), "Vacancy: "));
+            String s = bus.arrival;
+            TemporalAccessor ta = DateTimeFormatter.ISO_INSTANT.parse(s);
+            Instant i = Instant.from(ta);
+            Date d = Date.from(i);
+
+            System.out.println(d + " " + bus.code);
+          });
+          response.body();
+        } catch (IOException | InterruptedException e) {
+          e.printStackTrace();
+        }
+
+      });
+
+      return UpdatesListener.CONFIRMED_UPDATES_ALL;
+    });
+
+  }
 }
